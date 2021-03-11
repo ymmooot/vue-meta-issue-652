5e2ca4c209e914027a7ccf064c781da38896a96f
ymmooot <ryota.yama.9+github@gmail.com>
2021-03-11 22:04:16 +0900

    make it simple

--- plugins/meta.js
+++ plugins/meta.js
@@ -1,41 +1,24 @@
 import Vue from "vue";
 
 const vueMetaAppID = "vue-meta-app-1";
-const hid = "foo";
 
 const plugin = {
   install(V) {
-    let metaAppSetter
-
-    const rootMixin = {
-      beforeCreate() {
-        const isRoot = this.$root._uid === this._uid;
-        if (!isRoot) {
-          return;
-        }
-        const { set } = this.$meta().addApp(vueMetaAppID);
-        metaAppSetter = set
-      },
-    };
-
     const sampleMixin = {
-      mounted() {
-        metaAppSetter({
+      created() {
+        const { set } = this.$meta().addApp(vueMetaAppID);
+        set({
           script: [
             {
-              hid,
+              hid: 'test-script',
               type: "application/ld+json",
-              innerHTML: 'test' + this._uid,
+              json: { test: this._uid },
             },
           ],
-          __dangerouslyDisableSanitizersByTagID: {
-            [hid]: ["innerHTML"],
-          },
         })
       },
     };
 
-    V.mixin(rootMixin);
     V.mixin(sampleMixin);
   },
 };
